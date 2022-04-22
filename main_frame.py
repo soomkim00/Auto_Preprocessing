d608a5718aea6f8961ed9e2c27d0f29e302d4e46
Daegi Yeo <daegi0923@naver.com>
2022-04-22 17:32:46 +0900

    first commit

---
+++ main_frame.py
@@ -0,0 +1,11 @@
+from tkinter import *
+
+root = Tk()
+root.title("Auto Preprocessing")
+root.geometry("1080x720") #가로*세로 + (x좌표 + y 좌표)
+root.resizable(False, False) #x(너비), y(높이) 값 변경 불가 (창 크기 변경불가)
+
+
+
+root.mainloop()
+
