b3f51aa9d2472eb6940e5698bec84fcc59a0615d
Daegi Yeo <daegi0923@naver.com>
2022-04-22 17:39:19 +0900

    second commit

--- main_frame.py
+++ main_frame.py
@@ -5,7 +5,8 @@ root.title("Auto Preprocessing")
 root.geometry("1080x720") #가로*세로 + (x좌표 + y 좌표)
 root.resizable(False, False) #x(너비), y(높이) 값 변경 불가 (창 크기 변경불가)
 
-
+btn = Button(root, text = "hello")
+btn.pack()
 
 root.mainloop()
 
