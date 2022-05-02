9d8b73ca3f6c4ae6219e66493f85d05eff4ae82f
TangleDaegi <daegi0923@naver.com>
2022-05-02 16:51:18 +0900

    combobox

--- main_frame.py
+++ main_frame.py
@@ -1,4 +1,6 @@
 from tkinter import *
+import tkinter.ttk as ttk
+from read_csv import *
 
 root = Tk()
 root.title("Auto Preprocessing")
@@ -8,5 +10,16 @@ root.resizable(False, False) #x(너비), y(높이) 값 변경 불가 (창 크기
 btn = Button(root, text = "hello")
 btn.pack()
 
+print('path : ', os.getcwd())
+file_list = get_datalist()
+print(file_list)
+combobox = ttk.Combobox(root, height = 5, values = file_list)
+combobox.pack()
+def btncmd():
+    data_name = combobox.get()
+    print(combobox.get()) #선택된 값 표시
+    return data_name
+btn = Button(root, text = "select", command = btncmd)
+btn.pack()
 root.mainloop()
 
--- read_csv.py
+++ read_csv.py
@@ -7,24 +7,35 @@ pd.options.display.float_format = '{:,.2f}'.format #소숫점 자릿수
 pd.set_option('display.width', 85)
 pd.set_option('display.max_columns', 8)
 
-def show_datalist():
-    path = os.getcwd() #현재 폴더의 경로
-    data_path = path + "\data"
-    os.chdir(data_path) #현재 폴더의 /data로 디렉토리 변경
-    print(os.listdir()) #/data에 들어있는 파일명 출력
-    os.chdir(path)
 
-def read_csv():
+def get_datalist():
     path = os.getcwd() #현재 폴더의 경로
     data_path = path + "\data"
-    data_name = "\landtempssample.csv"
+    print(data_path)
+    os.chdir(data_path) #현재 폴더의 /data로 디렉토리 변경
+    data_list = os.listdir() #/data에 들어있는 파일명 출력
+    return data_list
+
+
+def read_csv(data_name):
+    path = os.getcwd() #현재 폴더의 경로
+    data_path = path + "\data"
+    #data_name = "\landtempssample.csv"
     data = pd.read_csv(data_path + data_name)
     return data
 
 
+def get_col_list(df):
+    col_list = df.columns.values.tolist()
+    return col_list
 
-data = read_csv()
-df = pd.DataFrame(data)
-print(df.head())
-col_list = df.columns.values.tolist()
-print(col_list)
+
+def select_col(df, col_list):
+    return df[col_list]
+
+
+# data = read_csv()
+# df = pd.DataFrame(data)
+# print(df.head())
+# col_list = df.columns.values.tolist()
+# print(col_list)
